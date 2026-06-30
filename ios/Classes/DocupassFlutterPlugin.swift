942ca09226b708b3f609059aae464cd1d2156ab1
Marktohark <marktohark@gmail.com>
2026-06-30 10:40:42 +0800

    clear

--- ios/Classes/DocupassFlutterPlugin.swift
+++
@@ -1,9 +0,0 @@
-import Flutter
-import UIKit
-
-public class DocupassFlutterPlugin: NSObject, FlutterPlugin {
-    public static func register(with registrar: FlutterPluginRegistrar) {
-        let factory = DocuPassViewFactory(messenger: registrar.messenger())
-        registrar.register(factory, withId: "com.idanalyzer.docupass/view")
-    }
-}
